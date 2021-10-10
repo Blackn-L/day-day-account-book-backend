184733e30634984ff9e0b997825f7309ed36b7c5
linfeng.ni <linfeng.ni@advance.ai>
2021-10-10 22:43:27 +0800

    feat:配置跨域白名单,全部

--- config/config.default.js
+++ config/config.default.js
@@ -1,19 +1,19 @@
 /* eslint valid-jsdoc: "off" */
 
-'use strict';
+"use strict";
 
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
-module.exports = appInfo => {
+module.exports = (appInfo) => {
   /**
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = (exports = {});
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1633087400237_4348';
+  config.keys = appInfo.name + "_1633087400237_4348";
 
   // add your middleware config here
   config.middleware = [];
@@ -21,6 +21,15 @@ module.exports = appInfo => {
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
+  };
+
+  // 设置跨域白名单，允许全部
+  config.security = {
+    csrf: {
+      enable: false,
+      ignoreJSON: true,
+    },
+    domainWhiteList: ["*"]
   };
 
   return {
