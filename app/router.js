2f106496cafd06e4322d70a95b91228e743fd4c2
linfeng.ni <linfeng.ni@advance.ai>
2021-12-03 10:14:02 +0800

    perf:接口遵循 restful 原则，修改改为 put，删除改为 delete

--- app/router.js
+++ app/router.js
@@ -9,11 +9,12 @@ module.exports = (app) => {
   router.post("/user/register", controller.user.register); // 注册
   router.post("/user/login", controller.user.login); // 登录
   router.get("/user/get", _jwt, controller.user.get_user_info); // 获取用户信息
-  router.post("/user/edit", _jwt, controller.user.edit); // 编辑用户信息
+  router.put("/user/edit", _jwt, controller.user.edit); // 编辑用户信息
+  router.put("/user/update_password", _jwt, controller.user.updatePassword); //  更新密码
   router.post("/bill/add", _jwt, controller.bill.add); // 增加账单
   router.get("/bill/list", _jwt, controller.bill.list); // 获取账单列表
   router.get("/bill/detail", _jwt, controller.bill.detail); // 获取账单详情
-  router.post("/bill/update", _jwt, controller.bill.update); // 更新账单
-  router.get("/bill/delete", _jwt, controller.bill.delete); // 删除账单
+  router.put("/bill/update", _jwt, controller.bill.update); // 更新账单
+  router.delete("/bill/delete", _jwt, controller.bill.delete); // 删除账单
   router.get("/bill/date", _jwt, controller.bill.date); // 获取整月账单统计数据
 };
